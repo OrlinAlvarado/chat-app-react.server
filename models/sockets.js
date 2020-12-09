24a711584c03f452c474183b2513f3a621e487fa
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-12-08 21:47:27 -0600

    Autenticacion de sockets

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,5 @@
+const { usuarioConectado, usuarioDesconectado } = require("../controllers/sockets");
+const { comprobarJWT } = require("../helpers/jwt");
 
 
 class Sockets {
@@ -11,8 +13,17 @@ class Sockets {
 
     socketEvents() {
         // On connection
-        this.io.on('connection', ( socket ) => {
+        this.io.on('connection', async ( socket ) => {
+            
+            const [ valido, uid ] = comprobarJWT( socket.handshake.query['x-token']);
+            if( !valido ) {
+                console.log('Socket no identificado');
+                return socket.disconnect();
+            }
+            
+            await usuarioConectado( uid );
             console.log('Cliente conectado');
+            
            //TODO: Validar el JWT
            //Si el token no es valido, desconectarlo
            
@@ -27,7 +38,10 @@ class Sockets {
            
            //TODO: Disconnect 
            //Marcar en la base de datos que el usaurio se desconecto
-           
+           socket.on('disconnect', async() => {
+               console.log('Usuario desconectado', uid );
+               await usuarioDesconectado( uid );
+           })
         
         });
     }
